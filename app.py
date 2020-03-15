2ab7b54fa975b92d1b39c95055b0ed2e8b4e9e8e
dukang <742976405@qq.com>
2020-03-16 00:02:54 +0800

    [根据前台选择的股票和日期完成相关性计算与图像绘制输出的接口]

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+#! /usr/bin/env python3
+#encoding: utf-8
 import os
 from flask import Flask,make_response, jsonify
 from flask_apscheduler import APScheduler
-from TushareConfig import base_stock_one_row_list
+from TushareConfig import base_stock_one_row_list, calculate_relation, read_base_stock
+import datetime
 
 
 # 配置定时任务
@@ -9,12 +12,12 @@ class Config(object):  # 创建配置，用类
     # 任务列表
     JOBS = [
         # {  # 第一个任务
-        #     'id': 'job1',
-        #     'func': '__main__:job_1',
-        #     'args': (1, 2),
-        #     'trigger': 'cron', # cron表示定时任务
-        #     'hour': 19,
-        #     'minute': 27
+        # 'id': 'job1',
+        # 'func': '__main__:job_1',
+        # 'args': (1, 2),
+        # 'trigger': 'cron', # cron表示定时任务
+        # 'hour': 19,
+        # 'minute': 27
         # },
         {  # 第二个任务，每隔5S执行一次
             'id': 'job2',
@@ -35,11 +38,50 @@ app.config.from_object(Config())  # 为实例化的flask引入配置
 
 
 # 获取股票列表,用于前端模糊匹配
-@app.route('/getBasicStock', methods=['GET', 'POST'])
-def basic_stock():
+# demo:http://127.0.0.1:8080/getBasicStock
+@app.route('/getBasicStock', methods=['POST'])
+def get_basic_stock():
     data = base_stock_one_row_list(os.path.dirname(app.instance_path).replace('\\', '/'))
     response = make_response(jsonify({'CodeStatus': 200, 'BasicStockOneRowList': data.tolist()}))
     return response
+
+
+# 计算相关性
+# 返回CalculateRelaionResultClass对象结构
+# params1 _first_stock:第一支股票的代码
+# params2 _second_stock:第二支股票的代码
+# params3 _time_type a = '一个月',b = '三个月',c = '一年',d = '三年'
+# demo:http://127.0.0.1:8080/getCalculateRelationResult/000001/000002/c
+@app.route('/getCalculateRelationResult/<_first_stock>/<_second_stock>/<any(a,b,c,d):_time_type>', methods=['POST'])
+def get_calculate_relation_result(_first_stock, _second_stock, _time_type):
+    # tushare的唯一标识比股票代码多加.SZ
+    _first_tushare_stock_code = _first_stock + '.SZ'
+    _second_tushare_stock_code = _second_stock + '.SZ'
+
+    # 通过_time_type计算需要提前多少天进行计算
+    _day_cut = int
+    if _time_type == 'a':
+        _day_cut = -30
+    elif _time_type == 'b':
+        _day_cut = -90
+    elif _time_type == 'c':
+        _day_cut = -365
+    else:
+        _day_cut = -1095
+
+    # 计算用户选择后的日期
+    _now_time = datetime.datetime.now()
+    _end_time = _now_time.strftime('%Y-%m-%d')    # 结束日期
+    # 选择要提前的天数
+    _calculate_time = _now_time + datetime.timedelta(days=_day_cut)
+    _start_time = _calculate_time.strftime('%Y-%m-%d')    # 开始日期
+    # 获取股票列表查询到股票的名称
+    _data = read_base_stock(os.path.dirname(app.instance_path).replace('\\', '/'))
+    _first_stock_name = _data[_data.ts_code == _first_tushare_stock_code].name[0]
+    _second_stock_name = _data[_data.ts_code == _second_tushare_stock_code].name[1]
+    # 进行两只股票的相关型和绘图
+    return_data = calculate_relation(_start_time, _end_time, _first_tushare_stock_code, _second_tushare_stock_code, _first_stock, _second_stock, _first_stock_name, _second_stock_name)
+    return make_response(return_data)
 
 
 if __name__ == '__main__':
--- utils/TushareConfig.py
+++ utils/TushareConfig.py
@@ -1,7 +1,13 @@
+#! /usr/bin/env python3
+#encoding: utf-8
 import tushare as ts
 import numpy as np
 import pandas as pd
 import os.path
+from io import BytesIO
+import base64
+import matplotlib.pyplot as plt  #  提供类matlab里绘图框架
+from flask import jsonify
 
 
 # 获取有Token的Tushare接口访问对象
@@ -29,12 +35,54 @@ def read_base_stock(root_path):
         # 如果不存在则创建目录
         # 创建目录操作函数
         os.makedirs(_path)
+        # 初次创建目录后主动刷新股票列表获取数据 否则无法读取
         refresh_base_stock(_file_path)
+    # 读取股票列表数据
     return pd.read_csv(_file_path)
 
 
 # 每一列所有数据进行格式化,用于前台进行模糊匹配展示
+# 格式化后的数据格式为:股票代码 股票地址 股票名称
 def base_stock_one_row_list(root_path):
     _df = read_base_stock(root_path)
     _df['result'] = _df['ts_code'].map(lambda x: x[:6]) + ' ' + _df['area'].map(str) + ' ' + _df['name'].map(str)
     return _df['result'].values
+
+
+# 获取数据
+# 两个时间段 开始时间 结束时间
+# 两只股票的代码
+def calculate_relation(_stock_start_data
+                       , _stock_end_data
+                       , _first_stock_code
+                       , _second_stock_code
+                       , _first_show_code
+                       , _second_show_code
+                       , _first_show_name
+                       , _second_show_name
+                       ):
+    # 通过接口获取时间段内每一天的收盘价
+    _df_first_stock = pro.daily(ts_code=str(_first_stock_code), start_date=str(_stock_start_data), end_date=str(_stock_end_data))
+    _df_second_stock = pro.daily(ts_code=str(_second_stock_code), start_date=str(_stock_start_data), end_date=str(_stock_end_data))
+    df = pd.concat([_df_first_stock.trade_date, _df_first_stock.close, _df_second_stock.close], axis=1, keys=['trade_date', _first_show_code, _second_show_code])  # 合并
+    df.ffill(axis=0, inplace=True)  # 填充缺失数据
+    # df.to_csv('md_gl.csv')    #保存数据到.csv文件
+    # pearson方法计算相关性
+    corr = df.corr(method='pearson', min_periods=1)
+    # 初始化相关性的值 相关性一个为1另一个为正常相关性 获取非1的数据
+    relation_result = str
+    if corr[_first_show_code][0] == 1:
+        relation_result = corr[_first_show_code][1]
+    else:
+        relation_result = corr[_second_show_code][0]
+    # 设置图像为600,650的像素
+    df.plot(figsize=(6, 6.5))
+    # plt.savefig('md_gl.png')  # 保存图像
+    # 写入内存
+    save_file = BytesIO()
+    plt.savefig(save_file, format='png')
+    # 转换base64并以utf8格式输出
+    save_file_base64 = base64.b64encode(save_file.getvalue()).decode('utf8')
+    plt.close()
+    # 构建json数据
+    return jsonify({'first_code': _first_show_code, 'second_code': _second_show_code, 'first_name': _first_show_name, 'second_name': _second_show_name, 'relation_result': relation_result, 'base64': save_file_base64, 'start_data': _stock_start_data, 'end_data': _stock_end_data})
